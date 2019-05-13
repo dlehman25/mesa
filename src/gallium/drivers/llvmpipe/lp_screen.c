8130e9bbc4198ace0d133581d0eaef311d89ce09
Daniel Lehman <dlehman@esri.com>
2019-05-13 13:21:22 -0700

    llvmpipe: Fake reporting of image_load_store
    Signed-off-by: Daniel Lehman <dlehman@esri.com>

--- src/gallium/drivers/llvmpipe/lp_screen.c
+++ src/gallium/drivers/llvmpipe/lp_screen.c
@@ -419,6 +419,8 @@ llvmpipe_get_shader_param(struct pipe_screen *screen,
             return PIPE_MAX_SHADER_SAMPLER_VIEWS;
          else
             return 0;
+      case PIPE_SHADER_CAP_MAX_SHADER_IMAGES:
+         return 16;
       default:
          return draw_get_shader_param(shader, param);
       }
